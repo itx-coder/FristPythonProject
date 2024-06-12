a7cb0445cb23e91aae6aaaa03a83a6e5a9101bb5
Hashir Raza <168596136+itx-coder@users.noreply.github.com>
2024-06-12 23:40:08 +0500

    Add files via upload

---
+++ FristAsign.py
@@ -0,0 +1,21 @@
+# Area of Triangle
+base = 14
+height = 7
+areaT = 0.5 * base * height
+print(areaT)
+
+#Area of Circle
+
+
+side1 = 20
+side2 = 15
+
+print(side1 * side2)
+
+# Area OF Circle
+
+import math
+radius = 7
+areaC = math.pi * radius * radius
+
+print(areaC)
