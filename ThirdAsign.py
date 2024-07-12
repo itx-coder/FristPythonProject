1fe3e48e6f2dfb2317725c7534ae226324719912
Hashir Raza <168596136+itx-coder@users.noreply.github.com>
2024-07-13 01:05:27 +0500

    Add files via upload

---
+++ ThirdAsign.py
@@ -0,0 +1,6 @@
+
+Frist_sentence = input("Enter your Sentence.")
+reverse_words = Frist_sentence.split()
+reverse_words.reverse()
+reverse_sentence = " ".join(reverse_words)
+print(reverse_sentence)
